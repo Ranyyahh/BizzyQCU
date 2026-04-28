67c7900a44c1dbd8e4d1089472b41ed0e2035f06
uykurtadrianlusteros-afk <uy.kurtadrian.lusteros@gmail.com>
2026-04-28 20:04:54 +0800

    Update 4.8.1

--- BizzyQCU/App_Start/RouteConfig.cs
+++ BizzyQCU/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,10 +14,11 @@ namespace BizzyQCU
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+    name: "Default",
+    url: "{controller}/{action}/{id}",
+    defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+);
+
         }
     }
 }
