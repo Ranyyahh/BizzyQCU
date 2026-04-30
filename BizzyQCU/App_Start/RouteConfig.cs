f8e15b583aa78e5f10649e62d0abb2fac7a32cb7
master-jed <tangonanjediael@gmail.com>
2026-05-01 01:47:00 +0800

    used the layout for tracking and history pages

--- BizzyQCU/App_Start/RouteConfig.cs
+++ BizzyQCU/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BizzyQCU
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Homepage", id = UrlParameter.Optional }
+                defaults: new { controller = "Tracking", action = "Tracking", id = UrlParameter.Optional }
             );
         }
     }
